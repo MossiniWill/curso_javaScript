1194a6187cb30b1a3c782a82a3838dca0268ede9
Wuilverson H. Mossini <Willmossini@gmail.com>
2023-03-05 16:49:49 -0300

    exemplos aula 1

---
+++ type_booleean.js
@@ -0,0 +1,4 @@
+const numero01 = 2;
+const numero02 = 2;
+
+console.log(numero01 === numero02);
---
+++ type_number.js
@@ -0,0 +1,19 @@
+//type number
+
+const meuNumero = 27;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica);
+
+//ponto flutuante
+
+const numeroPontoFluante = 2.9;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = meuNumero / numeroPontoFluante;
+
+return console.log(Math.round(novaOperacao));
