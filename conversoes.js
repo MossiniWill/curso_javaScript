e4a1a03108f53675ec846ea496f0ff7ac32c87e3
Mossini Will <willmossini@gmail.com>
2023-03-06 06:38:46 -0300

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dados
+//booleeanos
+
+
+//conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+// conversao explicita
