78c9ce66e427100738abed2ac1deeb7de0710905
Mossini Will <willmossini@gmail.com>
2023-03-09 06:58:29 -0300

    aula de funcoes e parametros

---
+++ parametros.js
@@ -0,0 +1,22 @@
+// parametros de funcao
+
+
+/*
+console.log(soma(10,5))
+
+function nomeIdade(nome, idade){
+    return `meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+console.log(nomeIdade("will", 31))*/
+
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+function multiplica(num1, num2){
+    return num1 * num2;
+}
+
+
+console.log(multiplica(soma(4, 5), soma(3, 3)))
